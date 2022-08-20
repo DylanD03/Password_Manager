435cc05b372dd9ec71cc3839e459983e9760c607
DylanD03 <90734935+DylanD03@users.noreply.github.com>
2022-08-20 15:41:06 -0600

    Implemented the interface for the main menu, and options. Need to check for erroneous inputs for the option menus.

--- Password_Manager.py
+++ Password_Manager.py
@@ -17,6 +17,21 @@ Security
 Extension
 	-Turn this password manager into a google extension.
 """
+def connect_to_Database():
+	"""
+	returns a cursor to the database named password_database.db
+	"""
+	connection = sqlite3.connect('password_database.db')
+	cur = connection.cursor()
+
+	return connection, cur
+
+
+
+def commit_to_Database(connection):
+	connection.commit()
+	connection.close()
+
 
 
 def initialize_Database():
@@ -24,13 +39,11 @@ def initialize_Database():
 	Creates the database file, 'password_database.db'
 	Creates the table named 'passwords' inside that database file.
 	"""
-	connection = sqlite3.connect('password_database.db')
-	cur = connection.cursor()
+	connection, cur = connect_to_Database()
 
 	cur.execute("CREATE TABLE passwords (username TEXT, password TEXT, website TEXT)")
 	
-	connection.commit()
-	connection.close()
+	commit_to_Database(connection)
 
 
 
@@ -40,13 +53,11 @@ def delete_Database():
 	i.e. deletes 'password_database.db'
 
 	"""
-	connection = sqlite3.connect('password_database.db')
-	cur = connection.cursor()
+	connection, cur = connect_to_Database()
 
 	cur.execute("DROP TABLE passwords")
 
-	connection.commit()
-	connection.close()
+	commit_to_Database(connection)
 
 
 
@@ -56,46 +67,56 @@ def add_Password(username, password, website):
 		add_Password(myusername, mypassword, github)
 
 	"""
-	connection = sqlite3.connect('password_database.db')
-	cur = connection.cursor()
+	connection, cur = connect_to_Database()
 
 	cur.execute("INSERT INTO passwords VALUES (?,?,?)", (username, password, website))
 
-	connection.commit()
-	connection.close()
+	commit_to_Database(connection)
+
+
+
+def delete_Password(row_Number):
+	"""
+	Deletes the targetted password from the database
+	"""
+
+	connection, cur = connect_to_Database()
+
+	cur.execute("DELETE FROM passwords WHERE rowid = (?)", row_Number)
+
+	commit_to_Database(connection)
+
 
 
 def show_all():
 	"""
 	Displays all username/passwords/website information
 	"""
-	connection = sqlite3.connect('password_database.db')
-	cur = connection.cursor()
+	connection, cur = connect_to_Database()
 
 	cur.execute("SELECT rowid, * FROM passwords")
 	items = cur.fetchall()
 	for item in items:
 		print(item)
 
-	connection.commit()
-	connection.close()
+	commit_to_Database(connection)
+
 	
+
+
 def show_website(queried_site):
 	"""
 	Displays username/password from a specific website:
 	connection = sqlite3.connect('password_database.db')
 	"""
-	connection = sqlite3.connect('password_database.db')
-	cur = connection.cursor()	
+	connection, cur = connect_to_Database()
 
 	cur.execute("SELECT * FROM passwords WHERE website = (?)", (queried_site,))
 	items = cur.fetchall()
 	for item in items:
 		print(item)
 
-	connection.commit()
-	connection.close()
+	commit_to_Database(connection)
 
 
 
-	
---
+++ interface.py
@@ -0,0 +1,151 @@
+import Password_Manager
+
+
+def print_Default_Header():
+	"""
+	Prints the header of the interface. 
+
+	"Password Manger v.x.y"
+
+	"""
+	print("-"*38)
+	print("\t" + "Password Manager v.1.0\n")
+
+
+def print_Default_Footer():
+	"""
+	Prints the Footer of the interface 
+
+
+	"""
+	print()
+	print("-"*38)
+
+
+
+def display_Option(options, option_Number):
+	print_Default_Header()
+
+	print(" Enter \'Q\' on keyboard to go back to Main Menu")
+	print(" Selected Option #" + str(option_Number) + ": " + options[option_Number-1])
+	print()
+	print(" to " + options[option_Number-1] + " Enter: ")
+
+
+
+	if option_Number == 1: # Add Password
+		print(" Username Password Website")
+		print()
+		print(" Example Usage: ")
+		print(" myusername mypassword github")
+
+	elif option_Number == 2: # Delete password
+		Password_Manager.show_all()
+		print()
+		print("Enter the row number of the password to be deleted")
+
+
+	elif option_Number == 3: # Show password from a site
+		print(" Website")
+		print()
+		print(" Example Usage: ")
+		print(" github")
+
+	elif option_Number == 4:
+		print(" \'1\'")
+
+	elif option_Number == 5:
+		print(" (y/n)\n")
+		print(" Example Usage: 'y'")
+		print(" note: ALL passwords will be deleted, and cannot be recovered")
+		print(" Input 'n' or 'q' if you change your mind")
+
+	print_Default_Footer()
+
+
+
+	
+def display_Main_Menu(options, cFlag = 1):
+	"""
+	prints the Main Menu of the interface, displaying the options for the user 
+	"""
+	print_Default_Header()
+	print(" Here are your options:\n")
+
+	# Displaying each option and it's coresponding reference number.
+	for i, option in enumerate(options):
+		# Example, "1 : Add Password"
+		print(" " + str(i+1) + " : " + option)
+
+	if not cFlag:
+		print("  Incorrect Input:")
+		print("  Example Usage: \"1\"")
+
+	print_Default_Footer()
+
+
+
+def safe_Input(options):
+
+	user_Input = input("Your Input: ") # TODO: Safe input function with exception handling. To cover erroneous inputs. Ex. no input 
+	
+	if user_Input == '':
+		return '', 0 # Incorrect usage
+
+	user_Input = int(user_Input)
+
+	if user_Input not in list(range(1, len(options)+1)):
+		return '', 0
+	else:
+		return user_Input, 1
+
+
+
+
+def main():
+	cFlag = 1
+	options = ["Add Password", "Delete Password", "Show password from a Site", "View All", "Delete All"]
+
+	while True:
+		
+		display_Main_Menu(options, cFlag)
+		user_Input, cFlag = safe_Input(options)
+
+		if not cFlag:
+			continue # Restart main menu but with example usage
+
+		display_Option(options, user_Input)
+		user_Input, cFlag = safe_Input(options)
+		
+
+
+
+
+
+		# # Correct Input usage from user
+		# while cFlag:
+		# 	display_Option(options, user_Input)
+		# 	user_Input, cFlag = safe_Input(options)
+
+		# 	if user_Input == 'q' or 'Q':
+		# 		break
+
+
+
+
+
+
+
+
+
+
+
+
+		break
+
+
+
+
+
+if __name__ == "__main__":
+	main()
