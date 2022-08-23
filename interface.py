143cd8afcb7e780c5e035fa0a61bc4fb150df7ca
DylanD03 <90734935+DylanD03@users.noreply.github.com>
2022-08-23 15:07:12 -0600

    v.1.2 version release

--- interface.py
+++ interface.py
@@ -22,9 +22,8 @@ Extension
 """
 
 
-
 	
-def display_Main_Menu(options):
+def display_Main_Menu(options, try_again = 0):
 	"""
 	prints the Main Menu of the interface, displaying the options for the user 
 """
@@ -38,8 +37,11 @@ def display_Main_Menu(options):
 		print(" " + str(i+1) + " : " + option) # Example, " 1 : Add Password"
 
 	print("\n Or enter 'q' to exit the program")
-	print("--------------------------------------")
 
+	if try_again:
+		print("\n Previous input is invalid, try again.")
+		print(" Example Usage: \"1\"")
+	print("--------------------------------------")
 
 
 
@@ -54,6 +56,7 @@ def safe_Input_Main(options):
 
 	returns
 	(string) user_Input : The option selected by the user. 
+
 	"""
 	while True: # Keep asking for input until a valid one is entered
 
@@ -62,129 +65,123 @@ def safe_Input_Main(options):
 		if user_Input == "Q" or user_Input == "q":
 			return user_Input # to exit program
 		if not user_Input.isdigit() or user_Input == '':
-			print("  Incorrect Input:")
-			print("  Example Usage: \"1\"")
+			display_Main_Menu(options, True)
 			continue # try again
 		# Input is a digit
 		if int(user_Input) not in list(range(1, len(options)+1)):
 			# try again. Input is not in the range of 1-n where n is the number of options
+			display_Main_Menu(options, True)
 			continue 
 
 		return user_Input # valid input
 
 
 
-def display_Option(options, option_Number, try_again = 0):
+
+def display_Option(option, try_again = 0):
 	"""
 	After user selects one of their options, display the respective interface.
 
 	"""
-	while True: # Keep displying that option screen until a valid input has been entered.
-		option = options[option_Number-1]
+	# while True: # Keep displying that option screen until a valid input has been entered.
 
-		print("--------------------------------------")
-		print("\t" + "Password Manager v.1.1\n")
-		print(" Enter \'q\' on keyboard to go back to Main Menu")
-		print(" Selected Option #" + str(option_Number) + ": " + option)
+	print("--------------------------------------")
+	print("\t" + "Password Manager v.1.1\n")
+	print(" Enter \'q\' on keyboard to go back to Main Menu")
+	print(" Selected Option : " + option)
+	print()
+	print(" to " + option + " Enter: ")
+
+	if option == "Add Password":
+		print(" Username Password Website")
 		print()
-		print(" to " + option + " Enter: ")
+		print(" Example Usage:\n myusername mypassword github")
+	elif option == "Delete Password": 
+		print()
+		print(" The website of the password to be deleted")
+		print(" Example Usage:\n Github")
+	elif option == "Show password from a Site": 
+		print(" Website")
+		print()
+		print(" Example Usage:\n Github")
+	elif option == "View All":
+		print(" \'1\'")
+	elif option == "Delete All":
+		print(" (y/n)\n")
+		print(" Example Usage: 'y'")
+		print(" note: ALL passwords will be deleted, and cannot be recovered")
+		print(" Input 'n' if you change your mind")
+	if try_again:
+		print("\n Previous input is invalid, try again")
+	print("--------------------------------------")
 
-		if option == "Add Password":
-			print(" Username Password Website")
-			print()
-			print(" Example Usage:\n myusername mypassword github")
-		elif option == "Delete Password": 
-			print()
-			print(" The website of the password to be deleted")
-			print(" Example Usage:\n Github")
-		elif option == "Show password from a Site": 
-			print(" Website")
-			print()
-			print(" Example Usage:\n Github")
-		elif option == "View All":
-			print(" \'1\'")
-		elif option == "Delete All":
-			print(" (y/n)\n")
-			print(" Example Usage: 'y'")
-			print(" note: ALL passwords will be deleted, and cannot be recovered")
-			print(" Input 'n' if you change your mind")
-
-		if try_again == 1:
-			print("\n Previous input is invalid, try again")
-		print("--------------------------------------")
-
-		cFlag = 0
-		user_Entry, cFlag = safe_Input_Options(option)
-
-		if not cFlag:
-			try_again == 1 
-			continue
-		break
-
-	return user_Entry # A valid entry has been entered and processed
 
 
 
 def safe_Input_Options(option): 
+	while True: # never ending loop until input is a valid entry
 	
-	user_Input = input("\nYour Input: ") 
+		user_Input = input("\nYour Input: ") 
 
-	if user_Input == "": # Invalid input for any operation.
-		return user_Input, 0
+		if user_Input == "": # Invalid input for any operation.
+			return user_Input
 
-	if user_Input == 'q' or user_Input == 'Q':
-		return user_Input, 1
+		if user_Input == 'q' or user_Input == 'Q':
+			return user_Input
 
-	if option == "Add Password":
-		if len(user_Input.split()) == 3: # valid formatting (Username Password Website)
-			inp = user_Input.split()
-			Password_Manager.add_Password(inp[0], inp[1], inp[2])
-			return user_Input, 1
 
-	elif option == "Delete Password": 
-		if Password_Manager.is_Valid_Website(user_Input):
-			Password_Manager.delete_Password(user_Input)
-			return user_Input, 1
+		if option == "Add Password":
+			if len(user_Input.split()) == 3: # valid formatting (Username Password Website)
+				inp = user_Input.split()
+				Password_Manager.add_Password(inp[0], inp[1], inp[2])
+				return user_Input
+		elif option == "Delete Password": 
+			if Password_Manager.is_Valid_Website(user_Input): # information for the website exists in database
+				Password_Manager.delete_Password(user_Input)
+				return user_Input
+		elif option == "Show password from a Site": 
+			if len(user_Input.split()) == 1: # websites must have no blank spaces in its name. 
+				if not Password_Manager.is_Valid_Website():
+					print(" There is no information associated with this website")
+				else:
+					Password_Manager.show_website(user_Input)
+				return user_Input
+		elif option == "View All":
+			if user_Input == '1':
+				Password_Manager.show_all()
+				return user_Input
+		elif option == "Delete All":
+			if user_Input == 'y':
+				Password_Manager.delete_Database()
+				Password_Manager.initialize_Database()
+				return user_Input
+			elif user_Input == 'n':
+				return user_Input
+		else:
+			display_Option(option, 1) # loop again until a valid input is used
+			continue 				  # readability
 
-	elif option == "Show password from a Site": 
-		if len(user_Input.split()) == 1:
-			Password_Manager.show_website(user_Input)
-			return user_Input, 1
-
-	elif option == "View All":
-		if user_Input == '1':
-			Password_Manager.show_all()
-			return user_Input, 1
-
-	elif option == "Delete All":
-		if user_Input == 'y':
-			Password_Manager.delete_Database()
-			Password_Manager.initialize_Database()
-			return user_Input, 1
-		elif user_Input == 'n':
-			return user_Input, 1
-
-	return user_Input, 0 
 	
-
 		
 def main():
 	options = ["Add Password", "Delete Password", "Show password from a Site", "View All", "Delete All"]
 	if not os.path.exists('./password_database.db'):
 		Password_Manager.initialize_Database() # Only creates database if it doesn't already exist.
 
-
 	while True:
-		display_Main_Menu(options) # Exits the main menu once a valid entry has been used.
+		display_Main_Menu(options)
 		user_Input = safe_Input_Main(options)
 		if user_Input == "q" or user_Input == "Q":
 			return # exits the program
-
-
-		user_Entry = display_Option(options, int(user_Input)) # Valid option is selected, display the corresponding interface
+		
+		option = options[int(user_Input)-1]
+		display_Option(option) # Valid option is selected, display the corresponding interface
+		user_Entry = safe_Input_Options(option) # also processes the tasks if the entry is valid
 		if user_Entry == "q" or user_Entry == "Q":
-			return # exits the program
-			 
+			continue # return to main menu
+		elif user_Entry == "n":
+			continue # return to main menu
+
 
 		while True:
 			print("--------------------------------------")
