7ca5df46be94bae4a11ff7fa117f855650c9710a
Dylan D <dndu@ualberta.ca>
2022-08-27 13:41:15 -0600

    Login interface has been implemented

--- Password_Manager.v.2.py
+++ Password_Manager.v.2.py
@@ -145,36 +145,5 @@ def replace_instance(current_user, website):
 	pass # future implementation
 
 
-# users database
-# https://www.geeksforgeeks.org/how-to-encrypt-and-decrypt-strings-in-python/
-# Cryptography
-def hash_password(password):
-
-	encoded_password = password.encode()
-	hashed_password = hashlib.sha256(encoded_password)
-
-	return hashed_password
-
-
-def generate_key():
-
-	key = Fernet.generate_key()
-	return key
-
-def encrypt_message(message, key):
-
-	fernet = Fernet(key)
-	encoded_message = message.encode()
-	encrypted_message = fernet.encrypt(encoded_message)
-
-	return encrypted_message
-
-def decrypt_mesage(encrypted_message, key):
-
-	fernet = Fernet(key)
-	encoded_message = fernet.decrypt(encrypted_message)
-	message = encoded_message.decode()
-
-	return message
 
 
